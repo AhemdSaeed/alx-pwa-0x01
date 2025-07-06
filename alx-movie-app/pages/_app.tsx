06c69dcb2d0194b507b086ccd94d0a7b55fd3c97
Ahmed Saeed <ahmed.m.saeed120@gmail.com>
2025-07-06 23:14:02 +0300

    init proj

---
+++ alx-movie-app/pages/_app.tsx
@@ -0,0 +1,14 @@
+import Layout from "@/components/layout/Layout";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
